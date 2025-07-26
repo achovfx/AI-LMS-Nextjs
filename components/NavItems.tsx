0c387d017a5ae84e425e1d25d06578921019e524
acho.vfx <x.gaming1386@gmail.com>
2025-07-26 17:20:25 +0330

    add navbar

---
+++ components/NavItems.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+const navItems = [
+    { label: "Home", href: "/" },
+    { label: "Companion", href: "/companion" },
+    { label: "Profile", href: "/my-journey" },
+]
+
+function NavItems() {
+
+    const pathname = usePathname();
+
+    return (
+        <nav className='flex items-center gap-4'>
+            {
+                navItems.map((item) => (
+                    <Link href={item.href} className={`${pathname === item.href && "text-primary font-semibold"}`} key={item.label}>{item.label}</Link>
+                ))
+            }
+        </nav>
+    )
+}
+
+export default NavItems
---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+// "use client";
+
+import { Link } from 'lucide-react'
+import Image from 'next/image'
+import React from 'react'
+import NavItems from './NavItems'
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <Link href="/">
+                <div className='flex item-center gap-2.5 cursor-pointer'>
+                    <Image src="/images/logo.svg" alt='logo' width={46} height={44} />
+                </div>
+            </Link>
+            <div className='flex items-center gap-8'>
+                <NavItems />
+                <p>Sign In</p>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
